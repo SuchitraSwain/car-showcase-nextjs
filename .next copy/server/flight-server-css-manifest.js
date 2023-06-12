67d8447354174fa0ee44b1dd19d61b19c3c340a6
Suchitra Swain <suchitraswain.2012gmail.com>
2023-06-12 20:59:14 +0200

    first commit

---
+++ .next copy/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/suchitraswain/Documents/Projects/project_next13_car_showcase/app/layout.tsx": [
+      "/Users/suchitraswain/Documents/Projects/project_next13_car_showcase/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/suchitraswain/Documents/Projects/project_next13_car_showcase/app/page": [
+      "/Users/suchitraswain/Documents/Projects/project_next13_car_showcase/app/globals.css"
+    ]
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# car-showcase-nextjs
+# Car Showcase NextJs App
+
+
+## Tech used - 
+1. NextJs
+2. Tailwind CSS
+3. Rapid API
+4. Typescript
+5. Vercel
+
+
+## How to start the project
+
+1. npm install
+2. npm run dev (localhost: 3000)
